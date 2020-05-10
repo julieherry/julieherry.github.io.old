238173786aefa1ad52286f33b0706691b99fada4
Kevin Antoine <5072452+kevin-ntn@users.noreply.github.com>
2020-05-10 19:37:32 +0200

    2020-05-10 19:37:32

--- .deploy.s8d0jfwefj0923wfes.sh
+++ .deploy.s8d0jfwefj0923wfes.sh
@@ -1,4 +1,5 @@
 cp -r ../julieherry/_site/ ./
+ls **/*.html | xargs -I{} sed -i -e 's/http:\/\/localhost:4000//g' {}
 git add .
 git commit -am "$(date '+%Y-%m-%d %H:%M:%S')"
 git push origin master
