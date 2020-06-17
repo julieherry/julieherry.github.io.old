efd3039eff5e638c4711fd35c33235cafefb081b
Kevin Antoine <5072452+kevin-ntn@users.noreply.github.com>
2020-06-17 23:57:41 +0200

    2020-06-17 23:57:41

--- .deploy.s8d0jfwefj0923wfes.sh
+++ .deploy.s8d0jfwefj0923wfes.sh
@@ -1,5 +1,8 @@
 cp -r ../julieherry/_site/ ./
 ls **/*.html | xargs -I{} sed -i -e 's/http:\/\/localhost:4000//g' {}
+sed -i -e 's/http:\/\/localhost:4000/http:\/\/julieherry.com/g' sitemap.xml && rm sitemap.xml-e
+sed -i -e 's/http:\/\/localhost:4000/http:\/\/julieherry.com/g' robots.txt && rm robots.txt-e
+rm **/*.html-e
 git add .
 git commit -am "$(date '+%Y-%m-%d %H:%M:%S')"
 git push origin master
