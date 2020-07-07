d534d67de9c129de98ad5c755686868e523ca660
Kevin Antoine <5072452+kevin-ntn@users.noreply.github.com>
2020-07-07 18:50:09 +0200

    2020-07-07 18:50:09

--- .deploy.s8d0jfwefj0923wfes.sh
+++ .deploy.s8d0jfwefj0923wfes.sh
@@ -1,8 +1,10 @@
+rm -rf 758-evergreen* apropos assets au-beau-milieu* images tags the-big* tmp 404.html index.html feed.xml
 cp -r ../julieherry/_site/ ./
 ls **/*.html | xargs -I{} sed -i -e 's/http:\/\/localhost:4000//g' {}
+rm **/*.html-e
 sed -i -e 's/http:\/\/localhost:4000/http:\/\/julieherry.com/g' sitemap.xml && rm sitemap.xml-e
 sed -i -e 's/http:\/\/localhost:4000/http:\/\/julieherry.com/g' robots.txt && rm robots.txt-e
-rm **/*.html-e
-git add .
-git commit -am "$(date '+%Y-%m-%d %H:%M:%S')"
+sed -i -e 's/http:\/\/localhost:4000/http:\/\/julieherry.com/g' feed.xml && rm feed.xml-e
+#git add .
+#git commit -am "$(date '+%Y-%m-%d %H:%M:%S')"
 #git push origin master
