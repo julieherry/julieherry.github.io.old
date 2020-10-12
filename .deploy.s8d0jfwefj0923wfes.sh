b0af6b7d8d6add7ee00be99bb805daa95ef9faaf
NkxxkN <kevin.antoine@protonmail.com>
2020-10-12 21:43:50 +0200

    2020-10-12 21:43:49

--- .deploy.s8d0jfwefj0923wfes.sh
+++ .deploy.s8d0jfwefj0923wfes.sh
@@ -1,7 +1,6 @@
 rm -rf 758-evergreen* apropos assets au-beau-milieu* images tags the-big* tmp 404.html index.html feed.xml
 cp -r ../julieherry/_site/ ./
-ls **/*.html | xargs -I{} sed -i -e 's/http:\/\/localhost:4000/http:\/\/julieherry.com/g' {}
-rm **/*.html-e
+ls **/*.html | xargs -I{} gsed -i -e 's/http:\/\/localhost:4000/http:\/\/julieherry.com/g' {}
 gsed -i  's/http:\/\/localhost:4000/http:\/\/julieherry.com/g' sitemap.xml
 gsed -i  's/http:\/\/localhost:4000/http:\/\/julieherry.com/g' robots.txt
 gsed -i  's/http:\/\/localhost:4000/http:\/\/julieherry.com/g' feed.xml
