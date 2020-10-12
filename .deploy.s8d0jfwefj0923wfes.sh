059085777498331eab88e94e7e8c2d27c9a2551e
NkxxkN <kevin.antoine@protonmail.com>
2020-10-12 23:24:43 +0200

    2020-10-12 23:24:43

--- .deploy.s8d0jfwefj0923wfes.sh
+++ .deploy.s8d0jfwefj0923wfes.sh
@@ -1,11 +1,11 @@
 rm -rf 758-evergreen* apropos assets au-beau-milieu* images tags the-big* tmp 404.html index.html feed.xml
 cp -r ../julieherry/_site/ ./
 
-gsed -i  's/http:\/\/localhost:4000/https:\/\/julieherry.com/g' sitemap.xml
-gsed -i  's/http:\/\/localhost:4000/https:\/\/julieherry.com/g' robots.txt
-gsed -i  's/http:\/\/localhost:4000/https:\/\/julieherry.com/g' feed.xml
+gsed -i  's/http:\/\/localhost:4000/https:\/\/www.julieherry.com/g' sitemap.xml
+gsed -i  's/http:\/\/localhost:4000/https:\/\/www.julieherry.com/g' robots.txt
+gsed -i  's/http:\/\/localhost:4000/https:\/\/www.julieherry.com/g' feed.xml
 git add  .
-git commit -am "$(date '+%Y-%m-%d %H:%M:%S')"
+# git commit -am "$(date '+%Y-%m-%d %H:%M:%S')"
 
 # ls **/*.html | xargs -I{}  gsed -i 's/http:\/\/localhost:4000/https:\/\/julieherry.com/g' {}
 # git push origin master
