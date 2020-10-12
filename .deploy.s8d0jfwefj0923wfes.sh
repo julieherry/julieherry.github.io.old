f37f497aebfd384453eb741fd74325fb9826218d
NkxxkN <kevin.antoine@protonmail.com>
2020-10-12 21:48:19 +0200

    2020-10-12 21:48:19

--- .deploy.s8d0jfwefj0923wfes.sh
+++ .deploy.s8d0jfwefj0923wfes.sh
@@ -1,9 +1,10 @@
 rm -rf 758-evergreen* apropos assets au-beau-milieu* images tags the-big* tmp 404.html index.html feed.xml
 cp -r ../julieherry/_site/ ./
-ls **/*.html | xargs -I{} gsed -i -e 's/http:\/\/localhost:4000/http:\/\/julieherry.com/g' {}
-gsed -i  's/http:\/\/localhost:4000/http:\/\/julieherry.com/g' sitemap.xml
-gsed -i  's/http:\/\/localhost:4000/http:\/\/julieherry.com/g' robots.txt
-gsed -i  's/http:\/\/localhost:4000/http:\/\/julieherry.com/g' feed.xml
+ls **/*.html | xargs -I{} gsed -i 's/http:\/\/localhost:4000/https:\/\/julieherry.com/g' {}
+rm **/*.html-e
+gsed -i  's/http:\/\/localhost:4000/https:\/\/julieherry.com/g' sitemap.xml
+gsed -i  's/http:\/\/localhost:4000/https:\/\/julieherry.com/g' robots.txt
+gsed -i  's/http:\/\/localhost:4000/https:\/\/julieherry.com/g' feed.xml
 git add  .
-git commit -am "$(date '+%Y-%m-%d %H:%M:%S')"
+# git commit -am "$(date '+%Y-%m-%d %H:%M:%S')"
 #git push origin master
